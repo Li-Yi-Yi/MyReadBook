d29a35816f94b890686e880cac0f49b63b0bf3a3
蔡成汉 <caichenghan@caichenghandeMacBook-Air.local>
2015-04-18 13:17:29 +0800

    创建

---
+++ MyBookReader/MyBookReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyBookReader
+//
+//  Created by 蔡成汉 on 15/4/18.
+//  Copyright (c) 2015年 LiYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyBookReader/MyBookReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyBookReader
+//
+//  Created by 蔡成汉 on 15/4/18.
+//  Copyright (c) 2015年 LiYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
